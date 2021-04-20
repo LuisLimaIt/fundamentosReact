3a86871ad26bd01ef644d170b70290b32179fcf8
LuisLimaIt <pls.luis@gmail.com>
2021-04-20 18:34:52 -0300

    Componente controlado

---
+++ src/components/formulario/Input.jsx
@@ -0,0 +1,27 @@
+import './Input.css'
+import React, { useState } from 'react';
+
+export default (props) => {
+    const [valor, setValor] = useState('inicial')
+
+    function quandoMudar(e){
+        setValor(e.target.value);
+    };
+
+    return(
+        <div>
+            <h1>{valor}</h1>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <input 
+                    className="input" 
+                    value={valor} 
+                    onChange={quandoMudar}/>
+                <input 
+                    className="input" value={undefined} />
+            </div>
+        </div>
+    );
+};
